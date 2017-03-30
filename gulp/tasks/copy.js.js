105d4bed616d0c3dafb13e3667aa668bc29ee669
Romuald <rarsenev@usetech.ru>
2017-03-30 16:54:06 +0300

    очень много переделал

---
+++ gulp/tasks/copy.js.js
@@ -0,0 +1,10 @@
+// задача - копирование файлов js из папки исходников в итоговую папку
+
+'use strict';
+
+module.exports = function() {
+  $.gulp.task('copy:js', function() {
+    return $.gulp.src('./source/js/**/*.*', { since: $.gulp.lastRun('copy:js') })
+      .pipe($.gulp.dest($.config.root + '/js')); 
+  });
+};

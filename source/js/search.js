1667a94eb9fbb34b69f9dc48f86767cc00d5ee99
Klepnev <klepnev-ea@yandex.ru>
2017-04-03 11:28:17 +0300

    Страница text, боковое меню.

--- source/js/search.js
+++ source/js/search.js
@@ -1,15 +1,27 @@
 var searchOpen = function() {
 	
   var searchTop = $('.search-top'),
+  		searchInput = $('#search-top'),
   		searchClose = $('#search-close'),
   		searchOpen = $('#seach-open');
 
   var openCloseSearch = function() {
   	searchTop.toggleClass('active');
+  	searchInput.val('Введите вопрос');
+  }
+
+  var clearSearch = function() {
+  	if(searchInput.val() === 'Введите вопрос') {
+  		searchInput.val('');
+  	}
   }  		
 
  	searchOpen.on('click', openCloseSearch);
  	searchClose.on('click', openCloseSearch);
+
+ 	/*очистка поля для ввода текста*/
+
+ 	searchInput.on('click', clearSearch);
 
 
 };

30d96e8ebe04d1094e556885fc65e9ec8f9b3afa
Klepnev <klepnev-ea@yandex.ru>
2017-04-07 20:05:42 +0300

    Подсветка инпутов

--- build/js/inputClean.js
+++ build/js/inputClean.js
@@ -1,21 +1,35 @@
 var inputClean = function() {
 
-  $('input').on('click', function(){
-
-        if($(this).val() !== '') {
+  $('.input').on('click', function(){
           $(this).val('');
-        };
   });
 
-  $('input').on('blur', function(){
+  $('.input').on('blur', function(){
 
     var that = $(this),
         thisVal = that.data('value-text');
 
         if(that.val() === '') {
           that.val(thisVal);
+          that.css('color', '#97a4b1')
         };
   });
+
+  $('.input').on('input', function(){
+        if($(this).val() !== '') {
+          $(this).css('color', '#000')
+        };
+  });
+
+
+  $('.input__datapicker').on('blur', function() {
+    $(this).css('color', '#000')       
+  });
+
+  $('.dropdown-toggle').on('click', function() {
+    $(this).css('color', '#000')       
+  });
+
 
 };
 

ccd97bcc917ed9432aa295f34dcb1da645d384d9
Klepnev <klepnev-ea@yandex.ru>
2017-04-10 10:23:50 +0300

    .

--- source/js/highlightTable.js
+++ source/js/highlightTable.js
@@ -12,19 +12,20 @@ var highlightTable = function() {
 			
 			var heightColumn = 0,
 			widthColumn = 0,
-			leftColumn = 0;
+			leftColumn = 0,
+			topColumn = 0;
 
 			heightColumn = $("#jobs-table").height();
 	    widthColumn = $(sel_tds[0]).width();
-	    leftColumn = $(sel_tds[0]).position().left;
+	    leftColumn = $(sel_tds[0]).offset().left;
+	    topColumn = $(sel_tds[0]).offset().top;
 
-			$('<div class="jobs-table-hight-column">.</div>').appendTo("#jobs-table");
-			//$().append();
+			$("body").append('<div class="jobs-table-hight-column">!!</div>');
 
 			$(".jobs-table-hight-column").height(heightColumn);
 			$(".jobs-table-hight-column").width(widthColumn + 32);
 			$(".jobs-table-hight-column").css({'left': leftColumn + 'px'});			
-
+			$(".jobs-table-hight-column").css({'top': topColumn + 'px'});			
 	});
 
 
